20417284aedad2fbe9f508429e7669f8c82e6f1f
Cristian Castillo <cristian.castillo.campos1993@gmail.com>
2020-08-31 11:48:23 -0600

    Add docs

---
+++ docs/precache-manifest.d6c05c432049ee2bb534704c618d9cf7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e5e3466df3297149c3f9f65330e3c5b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a007ecd3b308170c386e",
+    "url": "/static/css/main.eeae095e.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a007ecd3b308170c386e",
+    "url": "/static/js/main.054c73e5.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
